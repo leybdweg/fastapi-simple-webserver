401229e49790b8366c614e18a28fbbbc1bbf675f
danielweg <danielw@getvim.com>
2024-12-02 12:09:53 +0200

    clean up

--- main.py
+++ main.py
@@ -6,14 +6,3 @@ app = FastAPI(
 )
 
 app.include_router(api_router)
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-print(f"aaaa")
